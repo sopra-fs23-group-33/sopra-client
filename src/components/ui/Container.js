fa40612c1c491fef602f2ca0893a023875a42cb1
Maria Letizia Jannibelli <letizia.jannibelli@gmail.com>
2023-03-31 22:39:57 +0200

    added container

---
+++ src/components/ui/Container.js
@@ -0,0 +1,8 @@
+import "src/styles/ui/Container.scss";
+
+export const Container = props => (
+    <div {...props}
+         className={`container ${props.className ?? ''}`}>
+        {props.children}
+    </div>
+);
