c531cf07d40a632d0cbc2e4e57700a5946dc99f0
Maria Letizia Jannibelli <letizia.jannibelli@gmail.com>
2023-03-31 22:48:54 +0200

    changed header to game name

--- src/components/views/Header.js
+++ src/components/views/Header.js
@@ -13,8 +13,7 @@ import "styles/views/Header.scss";
  */
 const Header = props => (
   <div className="header container" style={{height: props.height}}>
-    <h1 className="header title">SoPra FS23 rocks with React!</h1>
-    <ReactLogo width="60px" height="60px"/>
+    <h1 className="header title">BULL V$. BEAR – THE BATTLE</h1>
   </div>
 );
 
