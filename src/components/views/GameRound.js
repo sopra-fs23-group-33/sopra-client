db5130dc4d0ddd6ccf069ef0d97a7c0f3f215c98
Maria Letizia Jannibelli <letizia.jannibelli@gmail.com>
2023-04-08 14:44:48 +0200

    work on design

--- src/components/views/GameRound.js
+++ src/components/views/GameRound.js
@@ -4,15 +4,25 @@ import BaseContainer from "../ui/BaseContainer";
 import "styles/views/GameRound.scss";
 import TextField from '@mui/material/TextField';
 import Button from "../ui/Button";
+import "styles/ui/Dashboard_ui/TableUserOverview.scss";
 
 const GameRound = () => {
     return (
         <BaseContainer>
+            <h1>Round 1/10</h1>
             <Grid container spacing={2}>
 
                 <Grid item xs={7}>
                     <div className="round wrapper">
                         chart ------------------------------
+                        -------------------------------------
+                        -------------------------------------
+                        -------------------------------------
+                        -------------------------------------
+                        -------------------------------------
+                        -------------------------------------
+                        -------------------------------------
+                        -------------------------------------
                         -------------------------------------
                         -------------------------------------
                         -------------------------------------
@@ -43,14 +53,37 @@ const GameRound = () => {
                                    variant="outlined"
                                    margin="normal"
                                    fullWidth={true} />
-                        <Button width="50%">Long</Button>
-                        <Button width="50%">Short</Button>
+                        <Button width="45%">Long</Button>
+                        <Button width="45%">Short</Button>
                     </div>
                 </Grid>
 
                 <Grid item xs={5}>
                     <div className="round wrapper">
                         My Powerups
+                        <div className="table">
+                                <thead>
+                                <tr>
+                                    <th>Powerup</th>
+                                    <th>num left</th>
+                                    <th></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>x10</td> <td>2</td> <td> <Button>Activate</Button> </td>
+                                    </tr>
+                                    <tr>
+                                        <td>x10</td> <td>2</td> <td> <Button>Activate</Button> </td>
+                                    </tr>
+                                    <tr>
+                                        <td>x10</td> <td>2</td> <td> <Button>Activate</Button> </td>
+                                    </tr>
+                                    <tr>
+                                        <td>x10</td> <td>2</td> <td> <Button>Activate</Button> </td>
+                                    </tr>
+                                </tbody>
+                        </div>
                     </div>
                 </Grid>
 
