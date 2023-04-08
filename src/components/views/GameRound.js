75cdb6eab3bcc9478d181552d2aa3cf31da5b775
Maria Letizia Jannibelli <letizia.jannibelli@gmail.com>
2023-04-08 14:23:19 +0200

    added basic blocks

--- src/components/views/GameRound.js
+++ src/components/views/GameRound.js
@@ -1,44 +1,59 @@
 import * as React from "react";
 import Grid from '@mui/material/Grid';
-import {Container} from "../ui/Container";
 import BaseContainer from "../ui/BaseContainer";
-import "styles/ui/TableContainer.scss";
+import "styles/views/GameRound.scss";
+import TextField from '@mui/material/TextField';
+import Button from "../ui/Button";
 
 const GameRound = () => {
     return (
         <BaseContainer>
             <Grid container spacing={2}>
-                <Grid item xs={8}>
-                    <Container>
-                        <div className="table-wrapper">
-                            <img
-                                scr=""
-                                alt="chart"
-                            />
-                        </div>
-                    </Container>
+
+                <Grid item xs={7}>
+                    <div className="round wrapper">
+                        chart ------------------------------
+                        -------------------------------------
+                        -------------------------------------
+                        -------------------------------------
+                        -------------------------------------
+                        -------------------------------------
+                        -------------------------------------
+                        -------------------------------------
+                        -------------------------------------
+
+                    </div>
                 </Grid>
-                <Grid item xs={4}>
-                    <Container>
-                        <div className="table-wrapper">
-                            xs=4
-                        </div>
-                    </Container>
+
+                <Grid item xs={5}>
+                    <div className="round wrapper">
+                        Timer
+                        <h1>18 sec left</h1>
+                    </div>
+                    <div className="round wrapper">
+                        My Balance
+                        <h1>53 coins</h1>
+                    </div>
                 </Grid>
-                <Grid item xs={4}>
-                    <Container>
-                        <div className="table-wrapper">
-                            xs=4
-                        </div>
-                    </Container>
+
+                <Grid item xs={7}>
+                    <div className="round wrapper">
+                        <TextField id="outlined-basic"
+                                   label="Enter betting amount..."
+                                   variant="outlined"
+                                   margin="normal"
+                                   fullWidth={true} />
+                        <Button width="50%">Long</Button>
+                        <Button width="50%">Short</Button>
+                    </div>
                 </Grid>
-                <Grid item xs={8}>
-                    <Container>
-                        <div className="table-wrapper">
-                            xs=8
-                        </div>
-                    </Container>
+
+                <Grid item xs={5}>
+                    <div className="round wrapper">
+                        My Powerups
+                    </div>
                 </Grid>
+
             </Grid>
         </BaseContainer>
 
