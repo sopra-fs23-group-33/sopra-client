183e97322facabad2fede85ec0c647a27a4a8340
Christian <christian.berger2@uzh.ch>
2023-04-11 11:17:21 +0200

    constants.js: - new file to store all constants, for example all game modes
    FormField.js:
    - new file to store the form field of the game lobby
    
    GameLobby.js:
    - some refactoring

--- src/components/views/GameLobby.js
+++ src/components/views/GameLobby.js
@@ -1,38 +1,11 @@
 import "styles/_theme.scss";
-// import {Link} from "react-router-dom";
 import React from "react";
-// import TextField from "@mui/material/TextField";
-// import Button from "../ui/Button";
-import FormField from "../ui/FormField";
+import FormField from "../ui/GameLobby/FormField";
 import 'styles/views/GameLobby.scss';
-import TextField from "@mui/material/TextField";
 import {MenuItem} from "@mui/material";
-// import UserList from "../ui/UserList";
-// import {useEffect, useState} from "react";
-// import {api_with_token, handleError} from "../../helpers/api";
+import {gameModes} from "../../helpers/constants";
 
-const gameModes = [
-    {
-        value: 'MULTIPLAYER',
-        label: 'Multi Player',
-    },
-    {
-        value: 'SINGLEPLAYER',
-        label: 'Single Player',
-    },
-    {
-        value: 'Without Power-Ups',
-        label: 'Without Power-Ups',
-    },
-    {
-        value: 'Without Events',
-        label: 'Without Events',
-    },
-    {
-        value: 'Demo',
-        label: 'Demo',
-    }
-];
+
 
 
 
@@ -68,55 +41,36 @@ const GameLobby = () => {
     return (
         <div className="primary-container">
             <div className="secondary-container">
-                <div className="formField container">
-                    <div className="formField form">
-                        <p>Set Game Room Name</p>
-                        <FormField
-                            id="outlined-required"
-                            label="Game Room Name"
-                        />
-                    </div>
-                </div>
-                <div className="formField container">
-                    <div className="formField form">
-                        <p>Set Maximum Number of Players</p>
-                        <FormField
-                            id="outlined-number"
-                            label="Maximum Number of Players"
-                            type="number"
-                        />
-                    </div>
-                </div>
-                <div className="formField container">
-                    <div className="formField form">
-                        <p>Set Maximum Number of Rounds</p>
-                        <FormField
-                            id="outlined-number"
-                            label="Maximum Number of Rounds"
-                            type="number"
-                        />
-                    </div>
-                </div>
+                <FormField
+                    header="Set Game Room Name"
+                    id="outlined-required"
+                    label="Game Room Name"
+                />
+                <FormField
+                    header="Set Maximum Number of Players"
+                    id="outlined-number"
+                    label="Maximum Number of Players"
+                    type="number"
+                />
+                <FormField
+                    header="Set Maximum Number of Rounds"
+                    id="outlined-number"
+                    label="Maximum Number of Rounds"
+                    type="number"
+                />
             </div>
             <div className="secondary-container">
-                <div className="formField container">
-                    <div className="formField form">
-                        <p>Set Game Mode</p>
-                        <TextField
-                            id="outlined-select-gameMode"
-                            select
-                            label="Select"
-                            defaultValue="MULTIPLAYER"
-                        >
-                            {gameModes.map((option) => (
-                                <MenuItem key={option.value} value={option.value}>
-                                    {option.label}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-                    </div>
-
-                </div>
+                <FormField
+                    header="Set Game Mode"
+                    id="outlined-select-gameMode"
+                    select="select"
+                    label="Select"
+                    list={gameModes.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                />
                 <h2>Players in Game Room</h2>
                 <div className="table-wrapper table">
                     {/*{users ? (*/}
