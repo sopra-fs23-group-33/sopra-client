b01dfbbbcb8fb8587c1b0be59067fd4c048057d5
Christian <christian.berger2@uzh.ch>
2023-04-14 12:22:17 +0200

    GameLobby.js: - two buttons added
    GameLobby.scss:
    - style for button added

--- src/components/views/GameLobby.js
+++ src/components/views/GameLobby.js
@@ -2,6 +2,7 @@ import "styles/_theme.scss";
 import React from "react";
 import 'styles/views/GameLobby.scss';
 import TableJoinedPlayers from "../ui/GameLobby/TableJoinedPlayers";
+import Button from "../ui/Button";
 
 const GameLobby = () => {
 
@@ -10,7 +11,18 @@ const GameLobby = () => {
             <div className="gl-primary-container">
                 <div className="gl-secondary-container">
                     <h2>Players in Game Room</h2>
+
                     <TableJoinedPlayers/>
+                    <div className="gl button-container">
+                        <Button
+                            width="100%">
+                            Start Game
+                        </Button>
+                        <Button
+                            width="100%">
+                            Delete Game Room
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
