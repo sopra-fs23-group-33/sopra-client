6d4edc45efb8179f12964eae01da7ae8cbad66ee
Christian <christian.berger2@uzh.ch>
2023-04-14 13:45:47 +0200

    GameLobby.js: - startGame and leaveGame to button added

--- src/components/views/GameLobby.js
+++ src/components/views/GameLobby.js
@@ -1,10 +1,39 @@
 import "styles/_theme.scss";
-import React from "react";
+import React, {useState} from "react";
 import 'styles/views/GameLobby.scss';
 import TableJoinedPlayers from "../ui/GameLobby/TableJoinedPlayers";
 import Button from "../ui/Button";
+import {api_with_token, handleError} from "../../helpers/api";
+import {useHistory} from "react-router-dom";
 
 const GameLobby = () => {
+
+    const [gameID] = useState(localStorage.getItem("gameID"));
+    const history = useHistory();
+
+    const startGame = async () => {
+        try {
+            await api_with_token().post("/games/" + gameID + "/start");
+
+            alert("Game started successfully.");
+        } catch (error) {
+            alert(`Game did not start: \n${handleError(error)}`);
+        }
+    };
+
+    const leaveGame = async () => {
+        try {
+            await api_with_token().post("/games/" + gameID + "/leave", {
+                gameID: gameID,
+                userID: localStorage.getItem("userID"),
+                username: localStorage.getItem("username")
+            });
+
+            history.push("/dashboard");
+        } catch (error) {
+            alert(`Player did not leave: \n${handleError(error)}`);
+        }
+    };
 
     return (
         <div className="gl-base-container">
@@ -15,11 +44,13 @@ const GameLobby = () => {
                     <TableJoinedPlayers/>
                     <div className="gl button-container">
                         <Button
-                            width="100%">
+                            width="100%"
+                            onClick={() => startGame()}>
                             Start Game
                         </Button>
                         <Button
-                            width="100%">
+                            width="100%"
+                            onClick={() => leaveGame()}>
                             Delete Game Room
                         </Button>
                     </div>
