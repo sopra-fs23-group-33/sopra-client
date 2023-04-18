045ab36b05275b3aff545127e8dca28fca4daffc
Maria Letizia Jannibelli <letizia.jannibelli@gmail.com>
2023-04-18 18:23:26 +0200

    fix tooltip background color

--- src/components/ui/GameRound&RoundResult_ui/Chart.js
+++ src/components/ui/GameRound&RoundResult_ui/Chart.js
@@ -9,7 +9,7 @@ const RenderLineChart = ({data}) => (
                         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                         <XAxis dataKey="date" tick={{ fill: "#ffffff" }} />
                         <YAxis type="number" domain={['dataMin - 0.1', 'dataMax + 0.1']} tick={{ fill: "#ffffff" }}/>
-                        <Tooltip />
+                        <Tooltip contentStyle={{backgroundColor: "#2e2954"}}/>
                     </LineChart>
             </ResponsiveContainer>
     </div>
