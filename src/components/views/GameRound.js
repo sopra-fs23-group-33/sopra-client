3850feebe66c25b25acf151c46d06745c2b577f3
Maria Letizia Jannibelli <letizia.jannibelli@gmail.com>
2023-04-18 21:44:12 +0200

    adjusted layout

--- src/components/views/GameRound.js
+++ src/components/views/GameRound.js
@@ -48,22 +48,6 @@ const GameRound = () => {
                         <h2>CHF/USD</h2>
                         <RenderLineChart data={data} />
                     </div>
-                </Grid>
-
-                <Grid item xs={5}>
-                    <div className="round wrapper">
-                        Timer
-                        <h1 style={{ fontSize: 50 }} align="center" >18</h1>
-                        <h1 align="center">seconds left</h1>
-                    </div>
-                    <div className="round wrapper">
-                        My Balance
-                        <h1 style={{ fontSize: 50 }} align="center">53</h1>
-                        <h1 align="center">coins</h1>
-                    </div>
-                </Grid>
-
-                <Grid item xs={7}>
                     <div className="round wrapper">
                         <TextField id="outlined-basic"
                                    label="Enter betting amount..."
@@ -73,36 +57,54 @@ const GameRound = () => {
                         <Button width="45%">Long</Button>
                         <Button width="45%">Short</Button>
                     </div>
+
                 </Grid>
 
                 <Grid item xs={5}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <div className="round wrapper">
+                                Timer
+                                <h1 style={{ fontSize: 50 }} align="center" >18</h1>
+                                <h1 align="center">secs left</h1>
+                            </div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className="round wrapper">
+                                My Balance
+                                <h1 style={{ fontSize: 50 }} align="center">53</h1>
+                                <h1 align="center">coins</h1>
+                            </div>
+                        </Grid>
+                    </Grid>
                     <div className="round wrapper">
                         My Powerups
                         <div className="table">
-                                <thead>
-                                <tr>
-                                    <th>Powerup</th>
-                                    <th>num left</th>
-                                    <th></th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>x10</td> <td>2</td> <td> <Button>Activate</Button> </td>
-                                    </tr>
-                                    <tr>
-                                        <td>x10</td> <td>2</td> <td> <Button>Activate</Button> </td>
-                                    </tr>
-                                    <tr>
-                                        <td>x10</td> <td>2</td> <td> <Button>Activate</Button> </td>
-                                    </tr>
-                                    <tr>
-                                        <td>x10</td> <td>2</td> <td> <Button>Activate</Button> </td>
-                                    </tr>
-                                </tbody>
+                            <thead>
+                            <tr>
+                                <th>Powerup</th>
+                                <th>num left</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>x10</td> <td>2</td> <td> <Button>Activate</Button> </td>
+                            </tr>
+                            <tr>
+                                <td>x10</td> <td>2</td> <td> <Button>Activate</Button> </td>
+                            </tr>
+                            <tr>
+                                <td>x10</td> <td>2</td> <td> <Button>Activate</Button> </td>
+                            </tr>
+                            <tr>
+                                <td>x10</td> <td>2</td> <td> <Button>Activate</Button> </td>
+                            </tr>
+                            </tbody>
                         </div>
                     </div>
                 </Grid>
+
 
             </Grid>
         </BaseContainer>
