5c7b7af9ff317f45b17c5c101ee54bc88e8b56f8
cedric-vr <cedric.vonrauscher@gmx.ch>
2023-04-21 09:43:09 +0200

    Leaderboard

--- src/components/views/Leaderboard.js
+++ src/components/views/Leaderboard.js
@@ -4,15 +4,18 @@ import {api_with_token, handleError} from "../../helpers/api";
 import {useEffect, useState} from "react";
 import TableList from "../ui/TableList";
 import PropTypes from "prop-types";
+import user from "../../models/User";
+import {useHistory} from "react-router-dom";
 
 
-const Player = ({player}) => (
+const Player = ({user}) => (
     <tr className="table user-overview row">
-        <td className="table user-overview">{player.rank}</td>
-        <td className="table user-overview">{player.username}</td>
-        <td className="table user-overview">{player.accountBalance}</td>
-        <td className="table user-overview">{player.numberOfBetsWon}</td>
-        <td className="table user-overview">{player.numberOfBetsLost}</td>
+        <td className="table user-overview">{user.rank}</td>
+        <td className="table user-overview">{user.username}</td>
+        <td className="table user-overview">{user.accountBalance}</td>
+        <td className="table user-overview">{user.numberOfBetsWon}</td>
+        <td className="table user-overview">{user.numberOfBetsLost}</td>
+        <td className="table user-overview">{user.winRate}</td>
     </tr>
 );
 
@@ -21,13 +24,14 @@ Player.propTypes = {
 };
 
 const Leaderboard = () => {
-    const [players, setPlayers] = useState(null);
+    const history = useHistory();
+    const [users, setUsers] = useState(null);
 
     useEffect(() => {
         async function fetchLeaderboard() {
             try {
                 const response = await api_with_token().get("/users/leaderboard");
-                setPlayers(response.data);
+                setUsers(response.data);
 
             } catch (error) {
                 console.error(`Something went wrong while fetching the leaderboard: \n${handleError(error)}`);
@@ -41,28 +45,47 @@ const Leaderboard = () => {
 
 
     return (
-        <div className="table-wrapper table">
-            {players ? (
-                <TableList>
-                    <thead>
-                    <tr>
-                        <th>Ranking</th>
-                        <th>Username</th>
-                        <th>Balance</th>
-                        <th>Rounds Won</th>
-                        <th>Rounds Lost</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {players.map(player => (
-                        <Player user={player} key={player.rank} />
-                    ))}
-                    </tbody>
-                </TableList>
-            ) : (
-                <p>Loading Leaderboard...</p>
-            )}
+        <div className="db container">
+            <div className="db primary-container">
+                <div className="db secondary-container">
+                    <h1>Leaderboard</h1>
+                    <div className="table-wrapper table">
+                        {users ? (
+                            <TableList>
+                                <thead>
+                                <tr>
+                                    <th>Ranking</th>
+                                    <th>Username</th>
+                                    <th>Balance</th>
+                                    <th>Rounds Won</th>
+                                    <th>Rounds Lost</th>
+                                    <th>Win Rate</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {users.map(user => (
+                                    <Player user={user} key={user.rank} />
+                                ))}
+                                </tbody>
+                            </TableList>
+                        ) : (
+                            <p>Loading Leaderboard...</p>
+                        )}
+                    </div>
+                    <br/>
+                    <br/>
+                    <div>
+                        <Button
+                            width="100%"
+                            onClick={() => history.push("/dashboard")}>
+                            Back to Dashboard
+                        </Button>
+                    </div>
+                </div>
+            </div>
         </div>
+
+
     );
 }
 
