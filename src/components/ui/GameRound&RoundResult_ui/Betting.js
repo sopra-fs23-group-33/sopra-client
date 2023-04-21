9ddfae3d827acc1063a1295e6a09da5748820f78
Christian <christian.berger2@uzh.ch>
2023-04-21 17:22:57 +0200

    Betting.js: first successful bet placement

--- src/components/ui/GameRound&RoundResult_ui/Betting.js
+++ src/components/ui/GameRound&RoundResult_ui/Betting.js
@@ -5,23 +5,35 @@ import {useState} from "react";
 import {api_with_token, handleError} from "../../../helpers/api";
 import {useTimeout} from "usehooks-ts";
 
+
+const BettingAmountField = props => {
+    return (
+        <TextField id="outlined-required"
+           label={props.label}
+           variant="outlined"
+           value={props.value}
+           onChange={e => props.onChange(e.target.value)}
+        />
+    );
+};
+
 const Betting = (props) => {
 
     const [playerID] = useState(localStorage.getItem("playerID"));
-    const [betType, setBetType] = useState(null);
-    const [betAmount, setBetAmount] = useState(0);
+    const [type, setType] = useState(null);
+    const [amount, setAmount] = useState(0);
 
     useTimeout(() => {
         const placeBet = async () => {
             try {
-                console.log(betType);
-                console.log(betAmount);
-                const requestBody = JSON.stringify({betType, betAmount});
+                console.log(type);
+                console.log(amount);
+                const requestBody = JSON.stringify({type, amount});
                 await api_with_token().put("/players/" + playerID + "/bet", requestBody);
                 alert("SUCCESS\nBet is placed.")
             } catch (error) {
-                console.log(betType);
-                console.log(betAmount);
+                console.log(type);
+                console.log(amount);
                 alert(`FAIL\n Something went wrong while placing the bet: \n${handleError(error)}`);
             }
         }
@@ -33,18 +45,15 @@ const Betting = (props) => {
 
     return (
         <div className="round wrapper">
-            <TextField
-                header="Set Game Room Name"
-                id="outlined-required"
-                label="Game Room Name"
-                helperText="At least one alphabetic character."
-                value={betAmount}
-                onChange={n => setBetAmount(n)}
-            />
+            <BettingAmountField
+                label="Betting Amount"
+                value={amount}
+                onChange={e => setAmount(e)}
+                />
             <Button width="20%"
-            onClick={() => setBetType("UP")}>Long</Button>
+            onClick={() => setType("UP")}>Long</Button>
             <Button width="20%"
-            onClick={() => setBetType("DOWN")}>Short</Button>
+            onClick={() => setType("DOWN")}>Short</Button>
         </div>
     );
 }
