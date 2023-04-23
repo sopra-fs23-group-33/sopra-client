79a7483a34a001f9094ae01075c9caf94a3601c3
cedric-vr <cedric.vonrauscher@gmx.ch>
2023-04-23 17:01:53 +0200

    Hover animation on Bull & Bear in Login/Register

--- src/components/ui/BullBearBackground.js
+++ src/components/ui/BullBearBackground.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import bullImage from "../../BullPicture.png";
+import bullImageGreen from "../../BullPictureGreen.png";
 import bearImage from "../../BearPicture.png";
+import bearImageRed from "../../BearPictureRed.png";
 
+// https://svgsilh.com/ms/4caf50/image/45977.html
+// https://svgsilh.com/ms/f44336/image/160226.html
 
 const BullBearBackground = () => {
+    const [bullIsHovered, bullSetIsHovered] = useState(false);
+    const [bearIsHovered, bearSetIsHovered] = useState(false);
+
     return (
         <div className="bull-bear">
-            <img src={bullImage} alt={"Bear"}/>
+            <img
+                className="bull"
+                src={bullIsHovered ? bullImageGreen : bullImage}
+                alt={"Bull"}
+                onMouseEnter={() => bullSetIsHovered(true)}
+                onMouseLeave={() => bullSetIsHovered(false)}
+            />
             <div className="filler"/>
-            <img src={bearImage} alt={"Bull"}/>
+            <img
+                className="bear"
+                src={bearIsHovered ? bearImageRed : bearImage}
+                alt={"Bear"}
+                onMouseEnter={() => bearSetIsHovered(true)}
+                onMouseLeave={() => bearSetIsHovered(false)}
+            />
         </div>
     )
 }
