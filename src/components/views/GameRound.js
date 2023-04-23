91ca947047441cb5cbe266a079c094730736176e
Christian <christian.berger2@uzh.ch>
2023-04-23 11:29:31 +0200

    GameRound.js: some bug fixes and some refactoring

--- src/components/views/GameRound.js
+++ src/components/views/GameRound.js
@@ -9,29 +9,6 @@ import TablePowerups from "../ui/GameRound&RoundResult_ui/TablePowerups";
 import {api_with_token, handleError} from "../../helpers/api";
 import {useEffect, useState} from "react";
 
-class Chart {
-    constructor(data = {}) {
-        this.fromCurrency = null;
-        this.toCurrency = null;
-        this.dates = null;
-        this.numbers = null;
-        Object.assign(this, data);
-    }
-}
-
-let dates = ["2023-05-01 17:10:00", "2023-05-01 17:15:00", "2023-05-01 17:20:00",
-    "2023-05-01 17:25:00", "2023-05-01 17:30:00", "2023-05-01 17:35:00",
-    "2023-05-01 17:40:00", "2023-05-01 17:45:00", "2023-05-01 17:50:00",
-    "2023-05-01 17:55:00", "2023-05-01 18:00:00", "2023-05-01 18:05:00",
-    "2023-05-01 18:10:00", "2023-05-01 18:15:00", "2023-05-01 18:20:00",
-    "2023-05-01 18:25:00", "2023-05-01 18:30:00", "2023-05-01 18:35:00",
-    "2023-05-01 18:40:00", "2023-05-01 18:45:00"]
-
-let numbers = [15.3603, 15.3465, 15.2549, 15.8121, 15.0046,
-    15.7888, 15.1042, 15.4487, 15.5519, 15.6120,
-    15.7228, 15.8942, 15.0201, 15.6789, 15.4573,
-    15.9156, 15.2381, 15.9812, 15.4430, 15.1759]
-
 
 const GameRound = () => {
 
@@ -82,6 +59,8 @@ const GameRound = () => {
         );
     }
 
+    let numbers = [];
+    let dates = [];
     if (chart) {
         numbers = chart.numbers;
         dates = chart.dates;
