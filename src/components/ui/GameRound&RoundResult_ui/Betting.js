228af3736b9e4eb7b2c9552601c19cc48d09a43d
Christian <christian.berger2@uzh.ch>
2023-04-27 10:05:07 +0200

    #56 MUI textfield replaced by input, in- and decrement button added, minor layout and style adjustments
    - the MUI textfield is not used anymore
    - increment and decrement function added to calculate the new betting amount if player clicks on in- or decrement button
    - size of long and short button changed
    - style adjustments in GameRound.scss

--- src/components/ui/GameRound&RoundResult_ui/Betting.js
+++ src/components/ui/GameRound&RoundResult_ui/Betting.js
@@ -4,29 +4,63 @@ import * as React from "react";
 import {useState} from "react";
 import {api_with_token, handleError} from "../../../helpers/api";
 import {InputAdornment} from "@mui/material";
+import PropTypes from "prop-types";
 
 
 const BettingAmountField = props => {
     return (
-        <TextField id="outlined-required"
-           label={props.label}
-           variant="outlined"
-           value={props.value}
-           onChange={e => props.onChange(e.target.value)}
-           InputProps={{
-               endAdornment: <InputAdornment position="end">coins</InputAdornment>,
-           }}
-        />
+        // <TextField id="outlined-required"
+        //    label={props.label}
+        //    variant="outlined"
+        //    value={props.value}
+        //    onChange={e => props.onChange(e.target.value)}
+        //    InputProps={{
+        //        endAdornment: <InputAdornment position="end">coins</InputAdornment>,
+        //    }}
+        // />
+        <div>
+            <label
+                className="round label"
+                >
+                Betting Amount
+            </label>
+            <input
+                className="round input"
+                placeholder="Please enter betting amount here..."
+                type="number"
+                min="1"
+                value={props.value}
+                onChange={e => props.onChange(e.target.value)}
+                >
+            </input>
+        </div>
     );
+};
+
+BettingAmountField.propTypes = {
+    value: PropTypes.string,
+    onChange: PropTypes.func
 };
 
 const Betting = () => {
 
     const [playerID] = useState(localStorage.getItem("playerID"));
     const [type, setType] = useState(null);
-    const [amount, setAmount] = useState();
+    const [amount, setAmount] = useState(null);
     const [disableLong, setDisableLong] = useState(false);
     const [disableShort, setDisableShort] = useState(false);
+
+    const handleClickIncrement = (increment) => {
+        if (amount === null) {
+            setAmount(increment.toString());
+        } else {
+            setAmount((parseInt(amount) + increment).toString());
+        }
+    };
+
+    const handleClickDecrement = (value) => {
+        setAmount((parseInt(amount) - value).toString());
+    };
 
     const handleClickLong = () => {
         void placeBet("UP");
@@ -56,22 +90,38 @@ const Betting = () => {
 
     return (
         <div className="round wrapper">
-            <BettingAmountField
-                label="Betting Amount"
-                value={amount}
-                onChange={e => setAmount(e)}
-                />
+            <div className="amount-container">
+                <Button
+                    className="long-button"
+                    width="7.5%"
+                    disabled={disableLong}
+                    onClick={() => {handleClickIncrement(1)}}>
+                    -1
+                </Button>
+                <BettingAmountField
+                    label="Betting Amount"
+                    value={amount}
+                    onChange={e => setAmount(e)}>
+                </BettingAmountField>
+                <Button
+                    className="long-button"
+                    width="7.5%"
+                    disabled={disableLong}
+                    onClick={() => {handleClickIncrement(1)}}>
+                    +1
+                </Button>
+            </div>
             <div className="long-short-container">
                 <Button
                     className="long-button"
-                    width="20%"
+                    width="40%"
                     disabled={disableLong}
                     onClick={() => {handleClickLong()}}>
                     Long
                 </Button>
                 <Button
                     className="short-button"
-                    width="20%"
+                    width="40%"
                     disabled={disableShort}
                     onClick={() => {handleClickShort()}}>
                     Short
