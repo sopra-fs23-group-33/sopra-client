2d7b2b9310d2422872b5388dede983fa2c537baa
Christian <christian.berger2@uzh.ch>
2023-05-06 14:44:34 +0200

    #78 major refactoring and "Leave Game" button displayed correctly
    - "Leave Game" button is shown properly now
    - Result.js added to create Result objects
    - if clauses removed by state variables which are initialized by new empty objects
    - if game status is CORRUPTED leaveGame() is called

--- src/components/views/GameResult.js
+++ src/components/views/GameResult.js
@@ -15,27 +15,32 @@ import {apiRequestIntervalGameRound} from "../../helpers/apiFetchSpeed";
 import "styles/views/GameResult.scss";
 import ActivatedPowerups from "../ui/GameRound&GameResult_ui/ActivatedPowerups";
 import InfoBox from "../ui/GameRound&GameResult_ui/InfoBox";
+import Game from "../../models/Game";
+import Chart from "../../models/Chart";
+import Player from "../../models/Player";
+import Result from "../../models/Result";
 
 const GameResult = () => {
 
     const history = useHistory();
-    const [gameID] = useState(localStorage.getItem("gameID"));
-    const [playerID] = useState(localStorage.getItem("playerID"));
-    const [gameStatus, setGameStatus] = useState(null);
-    const [chart, setChart] = useState(null);
-    const [betStatus, setBetStatus] = useState(null);
-    const [playerStatus, setPlayerStatus] = useState(null);
+    const gameID = localStorage.getItem("gameID");
+    const playerID = localStorage.getItem("playerID");
+    const [game, setGame] = useState(new Game());
+    const [chart, setChart] = useState(new Chart());
+    const [result, setResult] = useState(new Result());
+    const [player, setPlayer] = useState(new Player());
 
     useEffect(() => {
         const intervalId = setInterval(async () => {
             try {
-                const game = await api_with_token().get("/games/" + gameID + "/status");
-                setGameStatus(game.data);
-                if (gameStatus.status === "BETTING") {
+                const responseGame = await api_with_token().get("/games/" + gameID + "/status");
+                setGame(responseGame.data);
+                if (game.status === "BETTING") {
                     history.push("/game/round");
-                }
-                if (gameStatus.status === "OVERVIEW") {
+                } else if (game.status === "OVERVIEW") {
                     history.push("/game/session-result");
+                } else if (game.status === "CORRUPTED") {
+                    await leaveGame(history);
                 }
             } catch (error) {
                 console.log(error);
@@ -44,17 +49,17 @@ const GameResult = () => {
 
         return () => clearInterval(intervalId);
 
-    }, [gameStatus]);
+    }, [game]);
 
     useEffect(() => {
         async function updateData() {
             try {
                 // Get player data, e.g. type of current bet
                 const responsePlayer = await api_with_token().get("/players/" + playerID);
-                setPlayerStatus(responsePlayer.data);
-                // Get bet data, e.g. outcome of current bet
-                const responseBet = await api_with_token().get("/players/" + playerID + "/result");
-                setBetStatus(responseBet.data);
+                setPlayer(responsePlayer.data);
+                // Get result data, e.g. outcome of current bet
+                const responseResult = await api_with_token().get("/players/" + playerID + "/result");
+                setResult(responseResult.data);
                 // Get chart data
                 const responseChart = await api_with_token().get("/games/" + gameID + "/chart");
                 setChart(responseChart.data);
@@ -69,80 +74,60 @@ const GameResult = () => {
 
     }, []);
 
-    let rounds = <h2>Rounds played</h2>
-    let timerValue
-    let events
-    if (gameStatus) {
-        rounds = <h2>Result of Round {gameStatus.currentRoundPlayed}/{gameStatus.numberOfRoundsToPlay}</h2>
-        timerValue = gameStatus.timer
-        if (gameStatus.eventsActive === true) {
-            events = <TableEventsOccurred/>
-        }
+    let data
+    if (chart.numbers !== null && chart.dates !== null) {
+        data = chart.dates.map((date, index) => {
+            const time = date.split(' ')[1].split(':');
+            const formattedDate = time[0] + ':' + time[1];
+            return {date: formattedDate, value: chart.numbers[index]};
+        });
+    }
+
+    let move
+    let arrow = <TrendingFlatIcon sx={{ fontSize: 50}}/>
+    if (result.outcome === "UP") {
+        arrow = <TrendingUpIcon sx={{fontSize: 50, color: "#5bb44a"}}/>
+        move = <h2 className="result h2 long">Up</h2>
+    } else if (result.outcome === "DOWN") {
+        arrow = <TrendingDownIcon sx={{fontSize: 50, color: "#b44a4a"}}/>
+        move = <h2 className="result h2 short">Down</h2>
     }
 
     let profit
-    let bettingAmount
-    let move
-    let arrow = <TrendingFlatIcon sx={{ fontSize: 50}}/>
-    if (betStatus) {
-        bettingAmount = betStatus.bettingAmount
-        if (betStatus.outcome === "UP") {
-            arrow = <TrendingUpIcon sx={{fontSize: 50, color: "#5bb44a"}}/>
-            move = <h2 className="result h2 long">Up</h2>
-        } else if (betStatus.outcome === "DOWN") {
-            arrow = <TrendingDownIcon sx={{fontSize: 50, color: "#b44a4a"}}/>
-            move = <h2 className="result h2 short">Down</h2>
-        }
-        if (betStatus.profit >= 0) {
-            profit = <h2 className="result h1 profit">+{betStatus.profit}</h2>
-        } else if (betStatus.profit < 0) {
-            profit = <h2 className="result h1 loss">{betStatus.profit}</h2>
-        }
+    if (result.profit >= 0) {
+        profit = <h2 className="result h1 profit">+{result.profit}</h2>
+    } else if (result.profit < 0) {
+        profit = <h2 className="result h1 loss">{result.profit}</h2>
     }
 
     let betType
-    let accountBalance
-    if (playerStatus) {
-        if (playerStatus.typeOfCurrentBet === "UP") {
-            betType = (
-                <div>
-                    <h2 className="result h2 long">Long</h2>
-                </div>
-            )
-        } else if (playerStatus.typeOfCurrentBet === "DOWN") {
-            betType = (
-                <div>
-                    <h2 className="result h2 short">Short</h2>
-                </div>
-            )
-        }
-        accountBalance = playerStatus.accountBalance
+    if (player.typeOfCurrentBet === "UP") {
+        betType = (
+            <div>
+                <h2 className="result h2 long">Long</h2>
+            </div>
+        )
+    } else if (player.typeOfCurrentBet === "DOWN") {
+        betType = (
+            <div>
+                <h2 className="result h2 short">Short</h2>
+            </div>
+        )
     }
 
-    let content = <h2>Currency Pair</h2>
-    let numbers = []
-    let dates = []
-    if (chart) {
-        content = (
-            <h2>{chart.fromCurrency}/{chart.toCurrency}</h2>
-        );
-        numbers = chart.numbers;
-        dates = chart.dates;
+    let events
+    if (game.eventsActive === true) {
+        events = <TableEventsOccurred/>
     }
-
-    let data = dates.map((date, index) => {
-        const time = date.split(' ')[1].split(':');
-        const formattedDate = time[0] + ':' + time[1];
-        return { date: formattedDate, value: numbers[index] };
-    });
 
     return (
         <div className="round base-container">
-            <h2>{rounds}</h2>
+            <h2>Result of Round {game.currentRoundPlayed || "currentRoundPlayed"}/
+                {game.numberOfRoundsToPlay || "numberOfRoundsToPlay"}</h2>
             <Grid container spacing={2}>
                 <Grid item xs={7}>
                     <div className="round wrapper">
-                        {content}
+                        <h2>{chart.fromCurrency || "fromCurrency"}/{chart.toCurrency || "toCurrency"}</h2>
                         <RenderLineChart data={data} />
                     </div>
                     <Grid container spacing={2}>
@@ -150,7 +135,7 @@ const GameResult = () => {
                             <InfoBox
                             header="You bet"
                             event={betType}
-                            number={bettingAmount}
+                            number={result.bettingAmount}
                             unit="coins"
                             >
                             </InfoBox>
@@ -179,7 +164,7 @@ const GameResult = () => {
                         <Grid item xs={6}>
                             <InfoBox
                                 header="Next Round in"
-                                number={timerValue}
+                                number={game.timer}
                                 unit="secs"
                             >
                             </InfoBox>
@@ -187,17 +172,14 @@ const GameResult = () => {
                         <Grid item xs={6}>
                             <InfoBox
                                 header="My Balance"
-                                number={accountBalance}
+                                number={player.accountBalance}
                                 unit="coins"
                             >
                             </InfoBox>
                         </Grid>
                     </Grid>
-
                     <ActivatedPowerups />
-
                     <TableFinalRanking />
-
                     {events}
                     <div className="result button-container">
                         <Button
