34f4fd0bdc86fed01516ddf20ad60dbf417cb1c4
Christian <christian.berger2@uzh.ch>
2023-05-17 16:44:06 +0200

    fix #87 user gets pushed back to the previous page
    - user is not pushed to dashboard anymore

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -67,9 +67,9 @@ const About = () => {
             <div align="center">
                 <Button
                     className="leave-button"
-                    onClick={() => history.push("/dashboard")}
+                    onClick={() => history.go(-1)}
                 >
-                    Back to Dashboard
+                    Back
                 </Button>
             </div>
             </div>
