6d51108e604bdcd798461df5faeb2c0e795858a4
cedric-vr <cedric.vonrauscher@gmx.ch>
2023-05-21 10:08:49 +0200

    About: text fixes #87

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -12,34 +12,36 @@ const About = () => {
         <div className="round base-container">
             <br/>
             <h1 align="center">About</h1>
-                <br/>
-                <div className="round wrapper" >
-                <h2 align="center">About the Game</h2>
-                <p>
-                        <em><span className="bull-text">BULL</span> V<span className="dollar">$</span>. <span className="bear-text">BEAR</span> - The Battle</em> is a gamified
-                        application that lets you have fun while simultaneously making yourself familiar with trading, risk management and financial patterns. A crucial 
-                        aspect is that you are not confronted with synthetic data, but rather with real and up-to-date exchange currency courses - as fresh as some hours
-                        ago.
-                    </p>
+            <br/>
 
-                    <p>
-                        Gamification features such as power-ups, events, round structure and time pressure make for an exciting experience. Even more
-                        exciting if played in multiplayer mode with some friends. For a detailed description of how the game actually works, check
-                        out the Rulebook.
-                    </p>
-                <div align="center">
-                    <Button
-                        className="leave-button"
-                        onClick={() => history.push("/rulebook")}
-                    >
-                        More about the rules
-                    </Button>
-                </div>
+            <div className="round wrapper" >
+            <h2 align="center">About the Game</h2>
+            <p>
+                <em><span className="bull-text">BULL</span> V<span className="dollar">$</span>. <span className="bear-text">BEAR</span> - The Battle</em> is a gamified
+                application that lets you have fun while simultaneously making yourself familiar with trading, risk management and financial chart patterns. A crucial
+                aspect is that you are not confronted with synthetic data, but rather with real and up-to-date exchange currency courses - as fresh as some hours
+                ago.
+            </p>
+
+            <p>
+                Gamification features such as power-ups, events, round structure and time pressure make for an exciting experience. It's even more
+                exciting if played in multiplayer mode with some friends! For a detailed description of how the game works, check
+                out the Rulebook:
+            </p>
+
+            <div align="center">
+                <Button
+                    className="leave-button"
+                    onClick={() => history.push("/rulebook")}
+                >
+                    More about the rules
+                </Button>
+            </div>
 
             <h2 align="center">About the Project</h2>
             <p>
-                This online game came to life during the late winter and early spring of 2023, as a student project for the course <em>Softwarepraktikum</em> at the UZH, motivated by our interest
-                in the workings of the financial markets.
+                This online game came to life during the late winter and early spring of 2023, as a student project for the course <em>Softwarepraktikum</em> at the University of Zurich, motivated by our interest
+                in the workings of foreign exchange markets.
                 By now and in the near future, the game is free to play, without ads and at no cost for you. In the future however, we may consider keeping a share of the gainzzz you make
                 in your trades, as a brokerage fee.
             </p>
@@ -51,18 +53,18 @@ const About = () => {
 
             <h2 align="center">About the People Behind</h2>
             <p>
-            <em><span className="bull-text">BULL</span> V<span className="dollar">$</span>. <span className="bear-text">BEAR</span> - The Battle</em> was designed, developed and implemented by the following actual humans:
+            <em><span className="bull-text">BULL</span> V<span className="dollar">$</span>. <span className="bear-text">BEAR</span> - The Battle</em> was designed, developed and implemented by:
             </p>
             <ul>
-                <li><b>Christian Berger​</b> [@c-b123],</li>
-                <li><b>Maria Letizia Jannibelli ​</b> [@Marlet12],</li>
-                <li><b>Josep Cunquero Orts​</b> [@COrtsJosep],</li>
+                <li><b>Christian Berger </b> [@c-b123],</li>
+                <li><b>Maria Letizia Jannibelli </b> [@Marlet12],</li>
+                <li><b>Josep Cunquero Orts </b> [@COrtsJosep],</li>
                 <li><b>Stefan Richard Saxer </b> [@stsaxe], and</li>
-                <li><b>Cedric Egon Von Rauscher​</b> [@cedric-vr],</li>
+                <li><b>Cedric Egon Von Rauscher </b> [@cedric-vr]</li>
             </ul>
 
             <p>
-                under the supervision of Jerome Meier, and using as a starting point the client and server templates provided by Luis Torrejón, Roy Rutishauser and Dennys Huber. 
+                under the supervision of Jerome Meier, and using the client and server templates provided by Luis Torrejón, Roy Rutishauser and Dennys Huber as a starting point.
             </p>
             <div align="center">
                 <Button
